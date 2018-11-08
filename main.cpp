76d6fe28d35995889980044b45b884a0ed545429
Nomarroka27 <roman_duran27@outlook.com>
2018-11-07 23:14:00 -0600

    Carga procesos primer ajuste

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 string stats[3] = {"En espera\0", "Activo\0", "Detenido\0"};  //Estados posibles de procesos
 int PC=0;                //Contador de procesos
-const int MemMax=10;   //Memoria en kilobytes
+const int MemMax=100;   //Memoria en kilobytes
 
 class Process{           //Clase tipo proceso, utilizada para procesos en ejecucion y en espera
 	public:
@@ -38,24 +38,61 @@ class Process{           //Clase tipo proceso, utilizada para procesos en ejecuc
 		}   //Destructor
 };
 
+class ListProcE {
+	private:
+		Process *raiz;
+		int cont;
+	public:
+		ListProcE(Process *_raiz){
+			cont=0;
+			raiz = _raiz;
+		}
+		bool Push(Process *proc){
+			Process *temp=raiz;
+			if(cont<20){
+				if(raiz==NULL) raiz=proc;
+				else{
+					while(temp->sig) temp=temp->sig;
+					temp->sig=proc;
+				}
+				cont++;
+				return true;
+			}else{
+				return false;
+			}
+		}
+		Process * Pop(){
+			Process *temp;
+			if(cont>0){
+				temp=raiz;
+				raiz=raiz->sig;
+				cont--;
+				return temp;
+			}else{
+				return temp;
+			}
+		}
+		void to_show(){
+			Process *temp = raiz;
+			while ( temp != NULL ){
+				cout << temp->PID <<" : "<< temp->memoria <<endl;
+				temp = temp->sig ;
+			}
+			cout << "null" << endl;
+			return;
+		}
+		Process * Top(){
+			return raiz;
+		}
+};
+
 class ListProc {                       //Clase lista doblemente ligada
 	private:
 		Process *raiz;                 //Raiz
+		int cont=0;
 	public:
 		ListProc(Process *_raiz) {
 			raiz = _raiz;
-		}
-		void SetRaiz(Process *_raiz){
-			raiz=_raiz;
-		}
-		void push_front(Process *proc){
-			Process *nuevo=proc;
-			if (raiz != NULL) {
-				nuevo->sig = raiz;
-				raiz->ant = nuevo;
-			}
-			raiz = nuevo;
-			return;
 		}
 		void push_before(Process *proc, int prev){
 			Process *nuevo = proc;
@@ -86,22 +123,12 @@ class ListProc {                       //Clase lista doblemente ligada
 			cout << "null" << endl;
 			return;
 		}
-		int pop_front(){
-			Process *temp = NULL;
-			if ( raiz != NULL ) {
-				temp = raiz;
-				raiz = raiz->sig;
-			}
-			int x = temp->PID;
-			//delete temp;
-			return x;	
-		}
 		int pop(int val) {
 			Process *temp = raiz;
 			while(temp!=NULL && temp->memoria!=val) temp = temp->sig;
 			if(temp == raiz) raiz = raiz->sig;
 			else if ( temp->sig == NULL ) {
-				if (temp->PID == val ) temp->ant->sig = NULL;
+				if (temp->memoria == val ) temp->ant->sig = NULL;
 				else return 0;
 			} else if (temp != NULL) {
 				temp->ant->sig = temp->sig;
@@ -110,39 +137,28 @@ class ListProc {                       //Clase lista doblemente ligada
 			int x = temp->memoria;
 			return x;
 		}
-		void push_Back(Process *proc){
+		bool Primer_Ajuste(Process *proc){
 			Process *temp=raiz;
-			if(raiz!=NULL){
-				while(temp->sig){
-					temp=temp->sig;
-				}
-				temp->sig=proc;
-				proc->ant=temp;
-			}else raiz=proc;
-		}
-		void Primer_Ajuste(Process *proc){
-			Process *temp=raiz;
-			while(temp!=NULL || !(temp->memoria>proc->memoria && temp->PID!=0)){
+			while(temp->memoria<proc->memoria){
 				temp=temp->sig;
 			}
-			if(temp!=NULL){
-				int aux=(temp->memoria)-(proc->memoria);
-				this->push_before(proc,temp->memoria);
-				Process *paux=new Process(aux,0,0,0);
-				this->push_before(paux,proc->memoria);
-				this->pop(temp->memoria);
-			}else if((temp->memoria>proc->memoria && temp->PID!=0)){
-				
-			}
-		}
-		Process *Top(){
-			return raiz;
+			if(temp->memoria>proc->memoria){
+				if(temp!=NULL){
+					int aux=(temp->memoria)-(proc->memoria); 
+					push_before(proc,temp->memoria);
+					Process *paux=new Process(aux,0,0,0);
+					push_before(paux,proc->memoria);
+					pop(temp->memoria);
+				}
+				cont++;
+				return true;
+			}else return false;
 		}
 };
 
 Process *New_Process(){                 //Funcion que crea un nuevo proceso de manera aleatoria                              
 	int memoria,tiempo,status;
-	memoria = (rand()%30)+20; 
+	memoria = (rand()%20)+1; 
 	memoria+=(memoria%2); 
 	tiempo = (rand()%15)+5;
 	status = rand()%2;
@@ -154,106 +170,31 @@ int main(){
 	Process *ejec=new Process(MemMax,0,0,0);  //Proceso inicial con full memoria(raiz)
 	ListProc proc_ejec(ejec);                 //Objeto con ejec como raiz
 	Process *Esp=NULL,*New_proc;              //Proceso inicial a NULL(raiz)
-	ListProc proc_esp(Esp);                   //Objeto con esp como raiz
+	ListProcE proc_esp(Esp);                  //Objeto con esp como raiz
 	srand(time(NULL));
-	int num_process = (rand()%15)+15;              //No. De procesos de inicio
+	int num_process=20;                     //No. De procesos de inicio
 	for(int i=0; i<num_process; i++){              //Carga procesos iniciales
 		New_proc=New_Process();
-		proc_esp.push_Back(New_proc);
-	}int i=0;
-	proc_esp.to_show();
-	while(i<num_process){
-		New_proc=proc_esp.Top();
-		cout<<New_proc->PID<<endl;
-		proc_ejec.Primer_Ajuste(New_proc);
-		proc_esp.pop_front();
-		i++;
+		proc_esp.Push(New_proc);
 	}
+	int i=1;
+	do{
+		New_proc=proc_esp.Pop();  //Obtiene un proceso en espera
+		while(!proc_ejec.Primer_Ajuste(New_proc)){
+			
+		}
+		cout<<i++;
+		New_proc=New_Process();
+		proc_esp.Push(New_proc);
+	}while(1);
+	/*while(i<num_process){
+		New_proc=proc_esp.Pop();
+		if(!proc_ejec.Primer_Ajuste(New_proc)) break;
+		i++;
+	}*/
 	cout<<endl;
+	proc_esp.to_show();
 	proc_ejec.to_show();
 	return 0;
 }
 
-
-/*Hice un cambio :v borre el codigo*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#include <iostream>
-using namespace std;
-#include <time.h>
-#include <stdlib.h>
-
-string stats[3] = {"En espera\0", "Activo\0", "Detenido\0"};
-int PC=0;
-
-class Process{
-	public:
-	int memoria;
-	int PID;
-	int status;
-	int tiempo;
-		Process(int memoria, int PID, int status, int tiempo){
-			this->memoria = memoria;
-			this->PID = PID;
-			this->status = status;
-			this->tiempo = tiempo;
-		}
-		~Process(){}
-};
-
-int main(){
-	srand(time(NULL));
-	int new_process = (rand()%30)+15, memoria, status, tiempo;
-	Process *arr_proc = new Process(0,0,0,0);
-	Process *aux = arr_proc;
-	for(int i=0; i<new_process; i++, arr_proc++){
-		memoria = (rand()%300)+20;
-		memoria+=(memoria%2);
-		tiempo = (rand()%15)+5;
-		status = rand()%2;
-		arr_proc = new Process(memoria, ++PC, status, tiempo);
-		cout << arr_proc->PID << " " << "Proceso " << i+1 << " " << arr_proc->memoria << "k " <<arr_proc->tiempo << " " << stats[arr_proc->status] << endl;
-	}
-	arr_proc = aux;
-}
