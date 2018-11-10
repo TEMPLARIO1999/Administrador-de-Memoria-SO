6660bd6c8825682f0ca5e765a4fc0eef60971fdb
Nomarroka27 <roman_duran27@outlook.com>
2018-11-09 23:49:21 -0600

    Primer ajuste(version funcional)

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ class Process{           //Clase tipo proceso, utilizada para procesos en ejecuc
 		//Variables de un proceso, memoria,ID, Estado(activo,detenido,en espera),tiempo requerido de procesamiento
 		int memoria;
 		int PID;
-		int status;
-		int tiempo;
+		int status;   //0 = En ejecucion    1 = detenido    2 = libre 
+		int tiempo;  
 		Process(int memoria, int PID, int status, int tiempo){ //Constructor
 			sig=ant=NULL;
 			this->memoria = memoria;
@@ -33,10 +33,9 @@ class Process{           //Clase tipo proceso, utilizada para procesos en ejecuc
 };
 
 class ListProcE {
-	private:
+	public:
 		Process *raiz;
 		int cont;
-	public:
 		ListProcE(Process *_raiz){
 			cont=0;
 			raiz = _raiz;
@@ -69,22 +68,17 @@ class ListProcE {
 		void to_show(){
 			Process *temp = raiz;
 			while ( temp != NULL ){
-				cout << temp->PID <<" : "<< temp->memoria <<endl;
+				cout <<" PID "<< temp->PID <<" MEMORIA "<< temp->memoria <<" TIEMPO "<<temp->tiempo<<" SEG "<<endl;
 				temp = temp->sig ;
 			}
-			cout << "null" << endl;
 			return;
-		}
-		Process * Top(){
-			return raiz;
 		}
 };
 
 class ListProc {                       //Clase lista doblemente ligada
-	private:
+	public:
 		Process *raiz;                 //Raiz
 		int cont=0;
-	public:
 		ListProc(Process *_raiz) {
 			raiz = _raiz;
 		}
@@ -111,16 +105,17 @@ class ListProc {                       //Clase lista doblemente ligada
 		void to_show(){
 			Process *temp = raiz;
 			while ( temp != NULL ){
-				cout << temp->PID <<" : "<< temp->memoria << " tiempo : " << temp->tiempo << " seg "<<endl;
+				if(temp->PID) cout <<" PID "<< temp->PID <<" MEMORIA "<< temp->memoria <<" TIEMPO "<<temp->tiempo<<" SEG "<<endl;
+				else cout<<" ---------------- "<<temp->memoria<<" ---------------- "<<endl;
 				temp = temp->sig ;
 			}
-			cout << "null" << endl;
+			cout << endl;
 			return;
 		}
 		void rest_sec(){
 			Process *temp = raiz;
 			while ( temp != NULL ){
-				(temp->tiempo)--;
+				if(temp->status==0) (temp->tiempo)--;
 				temp = temp->sig ;
 			}	
 			return;
@@ -130,7 +125,7 @@ class ListProc {                       //Clase lista doblemente ligada
 			while ( temp != NULL ){
 				ant = temp;
 				temp = temp->sig ;
-				if(ant->tiempo <= 0) this->pop(ant->memoria);
+				if(ant->tiempo <= 0) ant->PID=0;
 			}	
 			return;
 		}
@@ -139,9 +134,9 @@ class ListProc {                       //Clase lista doblemente ligada
 			while(temp!=NULL && temp->memoria!=val) temp = temp->sig;
 			if(temp == raiz) raiz = raiz->sig;
 			else if ( temp->sig == NULL ) {
-				if (temp->memoria == val ) temp->ant->sig = NULL;
-				else return 0;
-			} else if (temp != NULL) {
+				temp->ant->sig = NULL;
+				return 0;
+			}else if (temp != NULL) {
 				temp->ant->sig = temp->sig;
 				temp->sig->ant = temp->ant;
 			} 
@@ -150,81 +145,94 @@ class ListProc {                       //Clase lista doblemente ligada
 		}
 		bool Primer_Ajuste(Process *proc){
 			Process *temp=raiz;
-			while(temp->memoria<proc->memoria){
+			while(temp){
+				if(temp->memoria>=proc->memoria){
+					if(temp!=NULL && temp->PID==0){
+						int aux=(temp->memoria)-(proc->memoria);
+						proc->status=0;
+						push_before(proc,temp->memoria);
+						Process *paux=new Process(aux,0,0,0);
+						if(temp->memoria!=proc->memoria) push_before(paux,proc->memoria);
+						pop(temp->memoria);
+						cont++;
+						return true;
+					}
+				}
 				temp=temp->sig;
 			}
-			if(temp->memoria>proc->memoria){
-				if(temp!=NULL){
-					int aux=(temp->memoria)-(proc->memoria); 
-					push_before(proc,temp->memoria);
-					Process *paux=new Process(aux,0,0,0);
-					push_before(paux,proc->memoria);
+			return false;
+		}
+		void PageMem(){
+			Process *temp=raiz;;
+			while(temp->sig){
+				if(temp->PID==0 && (temp->sig)->PID==0){
+					(temp->sig)->memoria=temp->memoria+(temp->sig)->memoria;
 					pop(temp->memoria);
 				}
-				cont++;
-				return true;
-			}else return false;
+				temp=temp->sig;
+			}
 		}
+		/*bool Mejor_ajuste(Process *proc){
+			Process *temp=raiz;	
+		}*/
 };
 
 Process *New_Process(){                 //Funcion que crea un nuevo proceso de manera aleatoria                              
 	int memoria,tiempo,status;
 	memoria = (rand()%20)+1; 
 	memoria+=(memoria%2); 
-	tiempo = (rand()%15)+5;
+	tiempo = (rand()%25)+5;
 	status = rand()%2;
 	Process *New_proc=new Process(memoria,++PC,status,tiempo);
 	return New_proc;
 }
 
+Process *ejec=new Process(MemMax,0,0,0);  //Proceso inicial con full memoria(raiz)
+ListProc proc_ejec(ejec);                 //Objeto con ejec como raiz
+Process *Esp=NULL,*New_proc;              //Proceso inicial a NULL(raiz)
+ListProcE proc_esp(Esp);                  //Objeto con esp como raiz
+
 void *ManageProcess(void *threadid) {
-    long tid;
-    tid = (long)threadid;
+    long *tid;
+    tid = (long*)threadid;
     
-    
-    Process *ejec=new Process(MemMax,0,0,0);  //Proceso inicial con full memoria(raiz)
-	ListProc proc_ejec(ejec);                 //Objeto con ejec como raiz
-	Process *Esp=NULL,*New_proc;              //Proceso inicial a NULL(raiz)
-	ListProcE proc_esp(Esp);                  //Objeto con esp como raiz
 	srand(time(NULL));
 	int num_process=20;                     //No. De procesos de inicio
 	for(int i=0; i<num_process; i++){              //Carga procesos iniciales
 		New_proc=New_Process();
 		proc_esp.Push(New_proc);
 	}
-	int i=1;
 	do{
 		New_proc=proc_esp.Pop();  //Obtiene un proceso en espera
+		cout <<"process"<< New_proc->PID <<" : "<< New_proc->memoria << " tiempo : " << New_proc->tiempo << " seg "<<endl;
 		while(!proc_ejec.Primer_Ajuste(New_proc)){
-			Sleep(5000);
+			Sleep(2000);
+			system("cls");
 			proc_ejec.rest_sec();
 			proc_ejec.quit_proc();
+			proc_ejec.PageMem();
 			proc_ejec.to_show();
+			cout <<"process "<< New_proc->PID <<" : "<< New_proc->memoria << " tiempo : " << New_proc->tiempo << " seg "<<endl;
 		}
-		cout<<i++;
+		Sleep(2000);
+		system("cls");
+		proc_ejec.rest_sec();
+		proc_ejec.quit_proc();
+		proc_ejec.PageMem();
+		proc_ejec.to_show();
 		New_proc=New_Process();
 		proc_esp.Push(New_proc);
 	}while(1);
-	/*while(i<num_process){
-		New_proc=proc_esp.Pop();
-		if(!proc_ejec.Primer_Ajuste(New_proc)) break;
-		i++;
-	}*/
-	cout<<endl;
-	proc_esp.to_show();
-	proc_ejec.to_show();
-	
-	
     pthread_exit(NULL);
 }
 
 void *ManageTime(void *threadid) {
-    long tid;
-    tid = (long)threadid;
-	
+    long *tid;
+    tid = (long*)threadid;
+	int n=0;
 	while(1){
-		cout << "Mientras la otra funcion maneja los procesos, yo puedo manejar las interrupciones.\n\n";
-		Sleep(10200);
+		
+		Sleep(2000);
 	}
 	
     pthread_exit(NULL);
